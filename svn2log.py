daca90e8f42ef7695d4950ad308fb958896b35ee
Marco Neumann <marco@crepererum.net>
2013-07-09 18:19:40 +0200

    Add svn parser

---
+++ svn2log.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+
+# command line arguments
+parser = argparse.ArgumentParser(description="Converts 'svn log' output to timeSht log file")
+parser.add_argument("--input", type=open, default="svn.log", help="Output from 'svn log'")
+parser.add_argument("--output", type=argparse.FileType("a"), default="timesht.log", help="timeSht log file")
+parser.add_argument("--user", required=True, help="Your SVN username")
+parser.add_argument("--year", type=int, required=True, help="The year that contains your magic month")
+parser.add_argument("--month", type=int, required=True, help="The month you are looking for")
+
+args = parser.parse_args()
+
+# read file and parse entries
+valid = False
+timestamp = ""
+messageParts = []
+delta = 0
+
+for line in args.input.readlines():
+	# next log entry?
+	if line.startswith("-------"):
+		if valid:
+			args.output.write("%s %s\n" % (timestamp, ", ".join(messageParts).rstrip()))
+		messageParts = []
+		valid = False
+		delta = 0
+
+	# parse entry header
+	if delta == 1:
+		headerParts = line.split(" | ")
+		user = headerParts[1]
+		time = headerParts[2]
+		if user == args.user:
+			timeParts = time.split(" ")
+			date = timeParts[0]
+			clock = timeParts[1]
+			dateParts = date.split("-")
+			clockParts = clock.split(":")
+			year = int(dateParts[0])
+			month = int(dateParts[1])
+			day = int(dateParts[2])
+			hour = int(clockParts[1])
+			if (year == args.year) and (month == args.month):
+				timestamp = "%i %i" % (day, hour)
+				valid = True
+
+	# parse log message
+	if delta > 2:
+		messageParts.append(line)
+
+	delta += 1
+
+args.input.close()
+
